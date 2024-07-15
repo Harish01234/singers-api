99ca2f148276f058a18263fbbfc9505a1da5db4c
Harish Malakar <harishmalakar19@gmail.com>
2024-07-15 22:46:12 +0530

    added all

---
+++ singers.js
@@ -0,0 +1,117 @@
+export const singers={
+    "pop_singers": [
+      {
+        "name": "Taylor Swift",
+        "birthdate": "1989-12-13",
+        "nationality": "American",
+        "albums": [
+          {
+            "title": "Fearless",
+            "release_year": 2008
+          },
+          {
+            "title": "1989",
+            "release_year": 2014
+          },
+          {
+            "title": "Lover",
+            "release_year": 2019
+          }
+        ],
+        "hit_songs": [
+          "Love Story",
+          "Shake It Off",
+          "Blank Space"
+        ]
+      },
+      {
+        "name": "Ariana Grande",
+        "birthdate": "1993-06-26",
+        "nationality": "American",
+        "albums": [
+          {
+            "title": "Yours Truly",
+            "release_year": 2013
+          },
+          {
+            "title": "Dangerous Woman",
+            "release_year": 2016
+          },
+          {
+            "title": "Thank U, Next",
+            "release_year": 2019
+          }
+        ],
+        "hit_songs": [
+          "Problem",
+          "No Tears Left to Cry",
+          "7 Rings"
+        ]
+      },
+      {
+        "name": "Ed Sheeran",
+        "birthdate": "1991-02-17",
+        "nationality": "British",
+        "albums": [
+          {
+            "title": "+",
+            "release_year": 2011
+          },
+          {
+            "title": "x",
+            "release_year": 2014
+          },
+          {
+            "title": "÷",
+            "release_year": 2017
+          }
+        ],
+        "hit_songs": [
+          "Shape of You",
+          "Perfect",
+          "Thinking Out Loud"
+        ]
+      },
+      {
+        "name": "Dua Lipa",
+        "birthdate": "1995-08-22",
+        "nationality": "British",
+        "albums": [
+          {
+            "title": "Dua Lipa",
+            "release_year": 2017
+          },
+          {
+            "title": "Future Nostalgia",
+            "release_year": 2020
+          }
+        ],
+        "hit_songs": [
+          "New Rules",
+          "Don't Start Now",
+          "Levitating"
+        ]
+      },
+      {
+        "name": "Billie Eilish",
+        "birthdate": "2001-12-18",
+        "nationality": "American",
+        "albums": [
+          {
+            "title": "When We All Fall Asleep, Where Do We Go?",
+            "release_year": 2019
+          },
+          {
+            "title": "Happier Than Ever",
+            "release_year": 2021
+          }
+        ],
+        "hit_songs": [
+          "Bad Guy",
+          "Everything I Wanted",
+          "Happier Than Ever"
+        ]
+      }
+    ]
+  }
+  
